9afbfbee3f3df784d870bdb801e79ecf03e1b5e7
Marco Buratto <m.buratto@lumit.it>
2019-12-19 10:20:30 +0100

    cryptsetup.hook fix

--- live-build/config/includes.chroot/etc/initramfs-tools/hooks/cryptsetup.hook.sh
+++ live-build/config/includes.chroot/etc/initramfs-tools/hooks/cryptsetup.hook.sh
@@ -19,9 +19,11 @@ copy_exec /sbin/cryptsetup
 copy_exec /sbin/dmsetup
 copy_exec /lib/cryptsetup/askpass
 
-#copy_exec /lib/x86_64-linux-gnu/libgcc_s.so*
 copy_exec /lib/*-linux-gnu/libgcc_s.so*
 
-mkdir run/cryptsetup
-chmod 700 run/cryptsetup
+if [ ! -d /run/cryptsetup ]; then 
+      mkdir /run/cryptsetup
+fi
+chmod 750 /run/cryptsetup
+
 
